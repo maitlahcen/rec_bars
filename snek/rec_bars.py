87bce9aef63a31588075ea9c36ced5b54a2d9dfd
Travis Cyronek <tcyronek@users.noreply.github.com>
2019-04-30 17:37:25 -0700

    Update rec_bars.py

--- snek/rec_bars.py
+++ snek/rec_bars.py
@@ -1,5 +1,5 @@
-# Author: Travis Cyronek
-# Date: 30 August 2017
+# Author:  Travis Cyronek
+# Date:    30 August 2017
 # Purpose: Add NBER recession bars to matplotlib graphs
 
 
@@ -9,7 +9,7 @@ import numpy as np
 import pandas_datareader as pdr
 
 
-def add_rec_bars(ax,begin=datetime.datetime(1854,12,1),end=datetime.datetime.today(),color='k',alpha=.2):
+def add_rec_bars(ax, begin=datetime.datetime(1854,12,1), end=datetime.datetime.today(), color='k', alpha=.2):
 
     """
     Description: This function adds recession bars to a matplotlib graph. The
@@ -28,16 +28,16 @@ def add_rec_bars(ax,begin=datetime.datetime(1854,12,1),end=datetime.datetime.tod
     """
 
     bot, top = ax.get_ylim()
-    rec_bars = pdr.DataReader('USREC','fred',begin,end)
-    rec_bars = np.multiply(rec_bars,top)
+    rec_bars = pdr.DataReader('USREC', 'fred', begin, end)
+    rec_bars = np.multiply(rec_bars, top)
     for i in range(len(rec_bars.index)):
         if rec_bars['USREC'][i] == 0:
             rec_bars['USREC'][i] += bot
 
-    ax.set_ylim([bot,top])
+    ax.set_ylim([bot, top])
     ax.fill_between(rec_bars.index,
                     np.ravel(rec_bars.as_matrix()),
                     np.zeros((len(rec_bars),))+bot,
-                    linewidth=0,color=color,alpha=alpha)
+                    linewidth=0, color=color, alpha=alpha)
 
     return ax
